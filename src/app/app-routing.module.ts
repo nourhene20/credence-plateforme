2482ff10bdda2ee2c5bfa0f525e4bdb72ecab217
nourhen youssfi <nourheneyoussfi03@gmail.com>
2026-05-17 22:21:39 +0100

    refactor: clean up routing by removing unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TextSubmissionInterfaceComponent } from './components/text-submission-interface/text-submission-interface.component';
 import { LoginInterfaceComponent } from './components/login-interface/login-interface.component';
 import { SignupInterfaceComponent } from './components/signup-interface/signup-interface.component';
-import { ReviewInterfaceComponent } from './components/review-interface/review-interface.component';
-import { HistoryInterfaceComponent } from './components/history-interface/history-interface.component';
+
 
 const routes: Routes = [
   { path: 'text-submission', component: TextSubmissionInterfaceComponent },
   { path: 'login', component: LoginInterfaceComponent },
   { path: 'signup', component: SignupInterfaceComponent },
-  { path: '', component: TextSubmissionInterfaceComponent },
-  {path: 'review', component: ReviewInterfaceComponent},
-  {path: 'history', component: HistoryInterfaceComponent}
-
+  { path: '', component: TextSubmissionInterfaceComponent }
+ 
 ];
 
 @NgModule({
